0274138f99ee62ebc6c9eed54c666b3ac14da1a2
Sara-E-Walker <50123761+Sara-E-Walker@users.noreply.github.com>
2022-09-08 21:00:22 -0700

    Update

--- Prosper/__main__.py
+++ Prosper/__main__.py
@@ -1,21 +1,61 @@
 # This is run when you call it as a "program"
+from CleanUp import PyCacheCleanUp
 
-from GUI import GUI
-GUI()
+import tkinter as tk
+from tkinter import HORIZONTAL, ttk
 
-from glob import glob
-from os import remove, removedirs, getcwd
-print("\nCommencing PyCache Clean Up")
-project_path = getcwd()
-py_cache_files = glob(project_path + "\\**\\*.pyc", recursive=True)
-py_cache_folders = glob(project_path + "\\**\\__pycache__", recursive=True)
-for file in py_cache_files:
-    try:
-        remove(file)
-    except FileNotFoundError:
-        print("Error removing .pyc files.")
-for directory in py_cache_folders:
-    try:
-        removedirs(directory)
-    except FileNotFoundError:
-        print("Error removing _pycache__ directories.")
+from OrderRecorder import OrderRecorder
+
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        
+        self.title('My Awesome App')
+        self.geometry('200x200')
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_rowconfigure(1, weight=5)
+        self.grid_rowconfigure(2, weight=1)
+
+class MainFrame(ttk.Frame):
+    def __init__(self, container):
+        super().__init__(container)
+
+        self.label = ttk.Label(self, text='Hello, Tkinter!')
+        self.label.grid(column=0, row=0)
+        
+        self.separator_frame = ttk.Frame(self)
+        self.separator_frame.grid(column=0, row=1, sticky='nsew', pady=5)
+
+        self.button = ttk.Button(self, text='Click Me')
+        self.button['command'] = self.OrderRecorderButton
+        self.button.grid(column=0, row=2)
+
+        self.grid(column=0, row=0)
+
+    def OrderRecorderButton(self):
+        OrderRecorder(self)
+
+
+if __name__ == "__main__":
+    app = App()
+    frame = MainFrame(app)
+    app.mainloop()
+# main_frame = ttk.Frame(root, width=200, height=200, style='MainMenu.TFrame')
+# main_frame_style = ttk.Style()
+# title = ttk.Label(main_frame, text="Pick menu:", style='Title.TLabel')
+# title_style = ttk.Style()
+# order_recorder_button = ttk.Button(main_frame, text="Record an order", default="active", command=OrderRecorderCallback)
+
+# root.title("Prosper")
+# root.geometry("200x200")
+# main_frame_style.configure('MainMenu.TFrame', background='aquamarine', borderwidth=5, relief='raised')
+# title_style.configure('Title.TLabel', background='aquamarine')
+
+# main_frame.grid(column=0, row=0, sticky=(N, W, E, S))
+# title.grid(column=0, row=0)
+# order_recorder_button.grid(column=0, row=1)
+
+
+# root.mainloop()
+PyCacheCleanUp()
